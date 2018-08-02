9ab61d44abaad7e2889c85bec4e9a26e76cb1468
Nathan Chang <Nathan.Chang@ibm.com>
2018-08-01 21:09:57 -0500

    updating new JSON models

--- app.js
+++ app.js
@@ -2,13 +2,12 @@
 const fs = require("fs"); // for JSON parsing
 const express = require("express"); // for endpoint management
 
-
-
 var set_data = fs.readFileSync("data/set.json");
 var author_data = fs.readFileSync("data/author.json");
 var set_json = JSON.parse(set_data);
 var author_json = JSON.parse(author_data);
 
+/*
 // debug
 console.log();
 console.log("*START*");
@@ -19,6 +18,7 @@ console.log("author: " + JSON.stringify(author_json, null, "\t"));
 console.log("---");
 console.log("*END*");
 console.log();
+*/
 
 // establish routing points
 const app = express();
--- filecontroller.js
+++ filecontroller.js
@@ -1,4 +1,16 @@
-
 /*
  * Listens for changes to the file system and updates JSON accordingly
  */
+
+const music_metadata = require("music-metadata");
+const util = require("util");
+
+
+
+music_metadata.parseFile("music/live_intern_hour_1532635200.mp3", {native: true})
+    .then(function (metadata) {
+        console.log(util.inspect(metadata, { showHidden: false, depth: null }));
+    })
+    .catch(function (err) {
+        console.error(err.message);
+    });
